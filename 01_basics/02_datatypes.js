c9f978f981f8236148714c999945590ab6672d24
pramodambati <110030966+pramodambati@users.noreply.github.com>
2024-02-05 11:17:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all jS code as newer version entrie files has treated in newer standard through the engine
+
+//  alert(3 + 3) // We are using nodejs, not browser
+
+// Just because the code has been executed and run does not mean everything is fine. Code has to be readable and also has to be future proof. If not what is the use of experience
+
+console.log(3 
+    + 3); console.log('Pramod')  // code redability  should be high
+
+let name = 'Pramod' // Stirng
+let age = 22 // Number
+let isLoggedIN = false // boolean
+let state; // value not defined
+// let state = null; we can assign it individually (representation of empty value)
+
+// number => 2 to power of 53
+// bigint trading, stock market 
+// string => ""
+// boolean => true/false
+// null Standard alone value and as well as datatype (its empty)
+// undefined => value not defined
+// symbol => unique, we talk more when we talk about react.
+
+// object 
+
+console.log(typeof "pramod");
+console.log(typeof age)
+console.log(typeof null); // Object (Interview question)
+console.log(typeof undefined); // undefined
